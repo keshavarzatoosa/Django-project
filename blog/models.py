e814255737e77489fcf94d720b403fa383029fab
keshavarzatoosa.dev <hel_sok_tikay@yahoo.com>
2025-01-03 08:36:28 -0800

    edit models in blog app

--- blog/models.py
+++ blog/models.py
@@ -6,11 +6,10 @@ from extensions.utils import jalali_converter
 from django.urls import reverse
 
 
-# managers
 class ArticleManager(models.Manager):
     def published(self):
         return self.filter(status="p")
-    
+   
 
 class CategoryManager(models.Manager):
     def active(self):
