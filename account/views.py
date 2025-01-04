c9f1affd7b8387b136f0d5b4fac28d26eb439af2
keshavarzatoosa.dev <hel_sok_tikay@yahoo.com>
2025-01-04 09:20:49 -0800

    add change password for user

--- account/views.py
+++ account/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
@@ -63,3 +63,7 @@ class Login(LoginView):
             return reverse_lazy("account:home")
         else:
             return reverse_lazy("account:profile")
+
+
+class PasswordChange(PasswordChangeView):
+    success_url = reverse_lazy("account:password_change_done")
